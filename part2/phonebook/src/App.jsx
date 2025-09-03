89457818b5c6d2b5437c96d86fc4a237ea1623e1
Otubusin Ademuyiwa (Innovate. Educate. Elevate.) <53128146+tay4real@users.noreply.github.com>
2025-09-03 21:58:49 +0100

    Exercise 2.9 completed

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,33 +1,66 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
   const [newName, setNewName] = useState('');
-  const [alreadyAdded, setAlreadyAdded] = useState(false);
+  const [newNumber, setNewNumber] = useState('');
+  const [search, setSearch] = useState('');
 
   const addName = (event) => {
     event.preventDefault();
 
     persons.forEach((element) => {
       if (element.name !== newName) {
-        setPersons(persons.concat({ name: newName }));
+        setPersons(persons.concat({ name: newName, number: newNumber }));
         setNewName('');
+        setNewNumber('');
       } else {
         alert(`${newName} is already added to phonebook`);
       }
     });
   };
 
-  const handleInputChange = (event) => {
+  const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
 
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    setSearch(event.target.value);
+  };
+
+  useEffect(() => {
+    search !== ''
+      ? setPersons(
+          persons.filter((person) =>
+            person.name.toLowerCase().includes(search.toLowerCase())
+          )
+        )
+      : setPersons(persons);
+  }, [search]);
   return (
     <div>
       <h2>Phonebook</h2>
+
+      <div>
+        filter shown with{' '}
+        <input type='text' onChange={handleSearch} value={search} />
+      </div>
+      <h2>add a new</h2>
       <form>
         <div>
-          name: <input onChange={handleInputChange} value={newName} />
+          name: <input onChange={handleNameChange} value={newName} />
+        </div>
+        <div>
+          number: <input onChange={handleNumberChange} value={newNumber} />
         </div>
         <div>
           <button type='submit' onClick={addName}>
@@ -39,7 +72,9 @@ const App = () => {
 
       <div>
         {persons.map((person, index) => (
-          <div key={index}>{person.name}</div>
+          <div key={index}>
+            {person.name} {person.number}
+          </div>
         ))}
       </div>
     </div>
